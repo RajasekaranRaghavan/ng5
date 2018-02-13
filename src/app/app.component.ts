3244e1f2ed62c6c75eb3266f008726a6e78583cb
RajasekaranRaghavan <rajeshraghav31@gmail.com>
2018-02-14 01:46:46 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+
+export class AppComponent {
+  title = 'My Angular-5 Application';
+}
